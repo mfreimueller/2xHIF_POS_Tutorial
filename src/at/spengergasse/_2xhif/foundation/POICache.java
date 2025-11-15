be98ea7cbbd458981ab954995867627e4d2a6383
Michael Freimüller <michael@mfreimueller.com>
2025-11-15 15:18:12 +0100

    implement solution for task 5: HashMap

--- src/at/spengergasse/_2xhif/foundation/POICache.java
+++ src/at/spengergasse/_2xhif/foundation/POICache.java
@@ -1,6 +1,9 @@
 package at.spengergasse._2xhif.foundation;
 
 import at.spengergasse._2xhif.domain.POI;
+import at.spengergasse._2xhif.foundation.exception.DuplicatedPOIException;
+
+import java.util.HashMap;
 
 /**
  * A cache for POIs that the user visited, in order to
@@ -8,9 +11,10 @@ import at.spengergasse._2xhif.domain.POI;
  */
 public class POICache {
     // TODO: we need a HashMap with appropriate Key-Value's
+    private final HashMap<Long, POI> cache;
 
     public POICache() {
-
+        cache = new HashMap<>();
     }
 
     /**
@@ -19,7 +23,15 @@ public class POICache {
      * @param poi The POI to add. Must not be null.
      */
     public void addPOI(POI poi) {
+        if (poi == null) {
+            throw new IllegalArgumentException("poi must not be null!");
+        }
 
+        if (cache.containsKey(poi.getId())) {
+            throw new DuplicatedPOIException(poi.getId());
+        }
+
+        cache.put(poi.getId(), poi);
     }
 
     /**
@@ -28,7 +40,15 @@ public class POICache {
      * @return The POI associated with the ID, otherwise false.
      */
     public POI findById(Long id) {
-        return null;
+        if (id == null) {
+            throw new IllegalArgumentException("id must not be null!");
+        }
+
+//        if (!cache.containsKey(id)) {
+//            return null;
+//        }
+
+        return cache.get(id);
     }
 
     /**
@@ -37,13 +57,31 @@ public class POICache {
      * @return True if a POI was removed, otherwise false.
      */
     public boolean removeById(Long id) {
-        return false;
+        if (id == null) {
+            throw new IllegalArgumentException("id must not be null!");
+        }
+
+//        if (!cache.containsKey(id)) {
+//            return false;
+//        }
+//
+//        cache.remove(id);
+//        return true;
+
+        // .remove returns the value associated with the key, or null if not found
+        return (cache.remove(id) != null);
     }
 
     /**
      * Should clear the cache and remove any stored POIs.
      */
     public void clear() {
+        // var == vom Compiler implizit erkannt: java.util.Set<Long>
+//        var keys = cache.keySet();
+//        for (var key : keys) {
+//            cache.remove(key);
+//        }
 
+        cache.clear();
     }
 }

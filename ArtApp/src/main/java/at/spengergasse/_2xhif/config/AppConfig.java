972f04288b619329b0dfc43e721d9781c9938f7d
Michael Freimueller <michael@mfreimueller.com>
2026-01-09 08:52:38 +0100

    teaching related changes

--- ArtApp/src/main/java/at/spengergasse/_2xhif/config/AppConfig.java
+++ ArtApp/src/main/java/at/spengergasse/_2xhif/config/AppConfig.java
@@ -1,21 +1,25 @@
 package at.spengergasse._2xhif.config;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
 
-public class AppConfig {
+public class AppConfig extends Object {
 
     public static AppConfig getInstance(String configFileName) throws IOException {
-        URL configURL = Thread.currentThread().getContextClassLoader().getResource(configFileName);
+//        URL configURL = Thread.currentThread().getContextClassLoader().getResource(configFileName);
+        String root = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        root = root + "config.properties";
 
-        if (configURL == null) {
+        /*if (configURL == null) {
             throw new FileNotFoundException(configFileName);
-        }
+        }*/
 
-        try (FileReader fileReader = new FileReader(configURL.getPath())) {
+//        FileInputStream fis;
+        try (FileReader fileReader = new FileReader(root)) {
             Properties properties = new Properties();
             properties.load(fileReader);
 
@@ -42,6 +46,9 @@ public class AppConfig {
 
     public int getMaxPOIs() {
         String maxPOIs = properties.getProperty("maxPOIs");
+
+//        int x = (int) "123";
+        int x = Integer.parseInt("123");
 
         try {
             return Integer.parseInt(maxPOIs);

0c347e055343b1b3c4f90bf303884e3a4da6521d
Michael Freimueller <michael@mfreimueller.com>
2026-05-04 16:13:24 +0200

    refactor Firma module to use compact class files (JDK25+)

--- Firma/src/main/java/Firma.java
+++ Firma/src/main/java/Firma.java
@@ -1,83 +1,89 @@
 import java.sql.*;
 
-public class Firma {
+void main() throws SQLException {
+    // actually, this calls for the 'strategy pattern' - https://refactoring.guru/design-patterns/strategy
+    final Connection connection = mariaDbConnection();
 
-    static void main() throws SQLException {
-        // final String connectionString = "jdbc:mysql://localhost:3306/firma";
-        final String connectionString = "jdbc:mariadb://localhost:3306/firma";
-        final Connection connection = DriverManager.getConnection(connectionString, "root", null);
+    try {
+        System.out.println("All customers:");
+        readCustomers(connection);
 
-        try {
-            System.out.println("All customers:");
-            readCustomers(connection);
+        System.out.println("\nOnly customers from Berlin:");
+        readCustomersFromCity(connection, "Berlin");
 
-            System.out.println("\nOnly customers from Berlin:");
-            readCustomersFromCity(connection, "Berlin");
+        System.out.println("\nSQL injection:");
+        readCustomersFromCitySqlInjectionVersion(connection, "' OR '1' = '1");
 
-            System.out.println("\nSQL injection:");
-            readCustomersFromCitySqlInjectionVersion(connection, "' OR '1' = '1");
-
-            System.out.println("\nSQL injection fixed:");
-            readCustomersFromCity(connection, "' OR '1' = '1");
-        } finally {
-            connection.close();
-        }
-
-        // is equivalent to...
-        // try (final Connection conn = DriverManager.getConnection(connectionString, "root", null)) {
-            // here be dragons...
-            // readCustomers(conn);
-        // }
+        System.out.println("\nSQL injection fixed:");
+        readCustomersFromCity(connection, "' OR '1' = '1");
+    } finally {
+        connection.close();
     }
 
-    static void readCustomers(final Connection connection) throws SQLException {
-        final Statement stmt = connection.createStatement();
-        try {
-            boolean isSuccess = stmt.execute("SELECT id, name, email, stadt FROM kunden;");
-            if (!isSuccess) {
-                System.err.println("SQL query returned no results.");
-                return;
-            }
+    // is equivalent to...
+    // try (final Connection conn = DriverManager.getConnection(connectionString, "root", null)) {
+        // here be dragons...
+        // readCustomers(conn);
+    // }
+}
 
-            // [0, 1, 2, 3] => 0xaf45 + sizeof(int32) => index 1: 1
+static void readCustomers(final Connection connection) throws SQLException {
+    final Statement stmt = connection.createStatement();
+    try {
+        boolean isSuccess = stmt.execute("SELECT id, name, email, stadt FROM kunden;");
+        if (!isSuccess) {
+            System.err.println("SQL query returned no results.");
+            return;
+        }
 
-            try (final ResultSet resultSet = stmt.getResultSet()) {
-                printResultSet(resultSet);
-            }
-        } finally {
-            stmt.close();
+        // [0, 1, 2, 3] => 0xaf45 + sizeof(int32) => index 1: 1
+
+        try (final ResultSet resultSet = stmt.getResultSet()) {
+            printResultSet(resultSet);
+        }
+    } finally {
+        stmt.close();
+    }
+}
+
+static void readCustomersFromCity(final Connection connection, final String city) throws SQLException {
+    final String sql = "SELECT * FROM kunden WHERE stadt = ?;";
+    try (final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        preparedStatement.setString(1, city);
+
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            printResultSet(resultSet);
         }
     }
+}
 
-    static void readCustomersFromCity(final Connection connection, final String city) throws SQLException {
-        final String sql = "SELECT * FROM kunden WHERE stadt = ?;";
-        try (final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setString(1, city);
-
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                printResultSet(resultSet);
-            }
+static void readCustomersFromCitySqlInjectionVersion(final Connection connection, final String city) throws SQLException {
+    final String sql = "SELECT * FROM kunden WHERE stadt = '" + city +"';";
+    try (final Statement statement = connection.createStatement()) {
+        try (ResultSet resultSet = statement.executeQuery(sql)) {
+            printResultSet(resultSet);
         }
     }
+}
 
-    static void readCustomersFromCitySqlInjectionVersion(final Connection connection, final String city) throws SQLException {
-        final String sql = "SELECT * FROM kunden WHERE stadt = '" + city +"';";
-        try (final Statement statement = connection.createStatement()) {
-            try (ResultSet resultSet = statement.executeQuery(sql)) {
-                printResultSet(resultSet);
-            }
-        }
+static void printResultSet(ResultSet resultSet) throws SQLException {
+    while (resultSet.next()) {
+        final int id = resultSet.getInt(1);
+        final String name = resultSet.getString(2);
+        final String email = resultSet.getString("email");
+        final String stadt = resultSet.getString("stadt");
+
+        System.out.printf("ID: %d | Name: %s | Email: %s | Stadt: %s\n", id, name, email, stadt);
     }
+}
 
-    static void printResultSet(ResultSet resultSet) throws SQLException {
-        while (resultSet.next()) {
-            final int id = resultSet.getInt(1);
-            final String name = resultSet.getString(2);
-            final String email = resultSet.getString("email");
-            final String stadt = resultSet.getString("stadt");
+static Connection mssqlConnection() throws SQLException {
+    final String connectionString = "jdbc:sqlserver://localhost:1433;databaseName=firma;trustServerCertificate=true";
+    return DriverManager.getConnection(connectionString, "sa", "SqlServer2019");
+}
 
-            System.out.printf("ID: %d | Name: %s | Email: %s | Stadt: %s\n", id, name, email, stadt);
-        }
-    }
-
+static Connection mariaDbConnection() throws SQLException {
+    // final String connectionString = "jdbc:mysql://localhost:3306/firma";
+    final String connectionString = "jdbc:mariadb://localhost:3306/firma";
+    return DriverManager.getConnection(connectionString, "root", null);
 }
